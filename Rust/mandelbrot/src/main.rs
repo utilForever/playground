e7ca0787c9ad43899c431c27f331caf36f855517
Chris Ohk <utilforever@gmail.com>
2024-05-23 21:06:57 +0900

    feat: Implement function 'render()'

--- Rust/mandelbrot/src/main.rs
+++ Rust/mandelbrot/src/main.rs
@@ -81,12 +81,23 @@ fn pixel_to_point(
 /// arguments specify points on the complex plane corresponding to the upper-left
 /// and lower-right corners of the pixel buffer.
 fn render(
-    _pixels: &mut [u8],
-    _bounds: (usize, usize),
-    _upper_left: Complex<f64>,
-    _lower_right: Complex<f64>,
+    pixels: &mut [u8],
+    bounds: (usize, usize),
+    upper_left: Complex<f64>,
+    lower_right: Complex<f64>,
 ) {
-    todo!()
+    assert_eq!(pixels.len(), bounds.0 * bounds.1);
+
+    for row in 0..bounds.1 {
+        for column in 0..bounds.0 {
+            let point = pixel_to_point(bounds, (column, row), upper_left, lower_right);
+
+            pixels[row * bounds.0 + column] = match escape_time(point, 255) {
+                Some(count) => 255 - count as u8,
+                None => 0,
+            }
+        }
+    }
 }
 
 /// Write the buffer `pixels`, whose dimensions are given by `bounds`,
