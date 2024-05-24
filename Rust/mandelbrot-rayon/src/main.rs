e90b38ac2a1866af3a0046737ac5eeafedecadac
Chris Ohk <utilforever@gmail.com>
2024-05-24 22:52:20 +0900

    feat: Replace code for render with 'rayon'

--- Rust/mandelbrot-rayon/src/main.rs
+++ Rust/mandelbrot-rayon/src/main.rs
@@ -1,6 +1,7 @@
 use image::codecs::png::PngEncoder;
 use image::{ExtendedColorType, ImageEncoder};
 use num::Complex;
+use rayon::prelude::*;
 
 use std::env;
 use std::fs::File;
@@ -138,7 +139,20 @@ fn main() {
 
     let mut pixels = vec![0; bounds.0 * bounds.1];
 
-    render(&mut pixels, bounds, upper_left, lower_right);
+    // Scope of slicing up `pixels` into horizontal bands.
+    {
+        let bands: Vec<(usize, &mut [u8])> = pixels.chunks_mut(bounds.0).enumerate().collect();
+
+        bands.into_par_iter().for_each(|(i, band)| {
+            let top = i;
+            let band_bounds = (bounds.0, 1);
+            let band_upper_left = pixel_to_point(bounds, (0, top), upper_left, lower_right);
+            let band_lower_right =
+                pixel_to_point(bounds, (bounds.0, top + 1), upper_left, lower_right);
+
+            render(band, band_bounds, band_upper_left, band_lower_right);
+        });
+    }
 
     write_image(&args[1], &pixels, bounds);
 }
